936a856ba834eb06d23b78194b187d7e5529df75
NoTimeInnovations <projects.notime@gmail.com>
2024-12-09 14:11:22 +0530

    fix icon path for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      }
+    })
   ],
   resolve: {
     alias: {
