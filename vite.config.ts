bbe6011e8cc9a43fb709fa845d06348b584c5a55
NoTimeInnovations <projects.notime@gmail.com>
2024-12-09 16:20:48 +0530

    fix viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'icon-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
