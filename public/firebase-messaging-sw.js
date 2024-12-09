e8458126ee96fbab9f091ca8a8606095e4a24ea0
NoTimeInnovations <projects.notime@gmail.com>
2024-12-09 16:14:27 +0530

    updated sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,8 +2,12 @@
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
 // Replace 10.13.2 with latest version of the Firebase JS SDK.
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
+);
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
@@ -11,11 +15,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-com
 firebase.initializeApp({
   apiKey: "AIzaSyCNmmrkaYgc_Y4DEwWfJyIp5RJjHF6_gSI",
   authDomain: "foodoffers-2cedb.firebaseapp.com",
-  databaseURL: "https://foodoffers-2cedb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://foodoffers-2cedb-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "foodoffers-2cedb",
   storageBucket: "foodoffers-2cedb.firebasestorage.app",
   messagingSenderId: "949249409105",
-  appId: "1:949249409105:web:56dc2befc107e3ad25b870"
+  appId: "1:949249409105:web:56dc2befc107e3ad25b870",
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -23,15 +28,16 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log(
-    '[firebase-messaging-sw.js] Received background message ',
+    "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-  // Customize notification here
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image
-  };
+  if (payload.notificationData) {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: payload.notification.image,
+    };
 
-  // self.registration.showNotification(notificationTitle, notificationOptions);
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,31 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true, // Enable PWA support in development mode for testing
       },
       manifest: {
-        name: "Crave",
-        short_name: "Crave",
-        description:
-          "Discover the best food deals and offers in your neighborhood. From cozy cafes to fine dining, we bring you exclusive discounts that will make your taste buds happy and your wallet happier",
-        theme_color: "#ffffff",
+        name: 'Crave',
+        short_name: 'Crave',
+        description: 'Discover the best food deals and offers in your neighborhood. From cozy cafes to fine dining, we bring you exclusive discounts that will make your taste buds happy and your wallet happier',
+        theme_color: '#ffffff',
         icons: [
           {
-            src: "icon-64x64.png",
-            sizes: "64x64",
-            type: "image/png",
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
@@ -39,10 +33,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   optimizeDeps: {
-    exclude: ["lucide-react"],
+    exclude: ['lucide-react'],
   },
 });
