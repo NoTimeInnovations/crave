5635354d8d7e70ecb0603d841f0d193ad65e10d5
ab-h-i-n <abhinshelly03@gmail.com>
2024-12-10 08:51:35 +0530

    fix dependency

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c7vs30uc9tg"
+    "revision": "0.rcpb6g4ap3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
